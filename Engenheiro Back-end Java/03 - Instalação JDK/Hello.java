24e005e863fb67e18da0f77846a99befeecaae8b
Eduardo Escudero <121754508+escudero-dev@users.noreply.github.com>
2023-01-02 15:51:11 -0300

    iniciando o curso de java

--- Engenheiro Back-end Java/03 - Instalação JDK/Hello.java
+++ Engenheiro Back-end Java/03 - Instalação JDK/Hello.java
@@ -1,5 +1,14 @@
 public class Hello {
     public static void main(String[] args) {
+
         System.out.println("Hello World!");
+        String nome = "Eduardo";
+        int idade = 15;
+
+        if(idade >= 18) {
+            System.out.println("Olá " + nome + "! Você tem " + idade + " anos e é maior de idade.");
+        } else {
+            System.out.println("Olá " + nome + "! Você tem " + idade + " anos e é menor de idade.");
+        }
     }
 }
