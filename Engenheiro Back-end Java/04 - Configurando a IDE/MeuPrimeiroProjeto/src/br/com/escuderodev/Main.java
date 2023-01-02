ed30cc48f3bec51ec268acd72b3b79b6dd790726
Eduardo Escudero <121754508+escudero-dev@users.noreply.github.com>
2023-01-02 16:56:17 -0300

    att

--- Engenheiro Back-end Java/04 - Configurando a IDE/MeuPrimeiroProjeto/src/br/com/escuderodev/Main.java
+++ Engenheiro Back-end Java/04 - Configurando a IDE/MeuPrimeiroProjeto/src/br/com/escuderodev/Main.java
@@ -2,8 +2,9 @@ package br.com.escuderodev;
 
 public class Main {
     public static void main(String[] args) {
+        String nome = "Eduardo Escudero";
 
-        System.out.println("Hello world!");
+        System.out.println("Hello " + nome + "!");
     }
 }
 
