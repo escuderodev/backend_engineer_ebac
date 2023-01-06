ba3303f990fefc963b0048d5e8089abeeb480572
Eduardo Escudero <escudero.ti@gmail.com>
2023-01-06 14:43:36 -0300

    continuação do curso de java

---
+++ Engenheiro Back-end Java/10 - Controle de fluxos/ExercicioMod10/src/br/com/escuderodev/aula/For.java
@@ -0,0 +1,12 @@
+package br.com.escuderodev.aula;
+
+public class For {
+
+    public static void main(String[] args) {
+
+//        contando até 10
+        for (int i = 1; i <= 10 ; i++) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ Engenheiro Back-end Java/10 - Controle de fluxos/ExercicioMod10/src/br/com/escuderodev/aula/While.java
@@ -0,0 +1,11 @@
+package br.com.escuderodev.aula;
+
+public class While {
+    public static void main(String[] args) {
+        int count = 0;
+        while (count <= 10) {
+            System.out.println(count);
+            count++;
+        }
+    }
+}
