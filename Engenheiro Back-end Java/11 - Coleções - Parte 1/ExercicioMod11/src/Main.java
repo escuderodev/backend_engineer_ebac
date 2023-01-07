b3bec8c1e3249709730f44035e5929f1c4517a05
Eduardo Escudero <escudero.ti@gmail.com>
2023-01-07 16:14:03 -0300

    concluindo exercício mod 11

---
+++ Engenheiro Back-end Java/11 - Coleções - Parte 1/ExercicioMod11/src/Main.java
@@ -0,0 +1,37 @@
+import br.com.escuderodev.models.Aluno;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Aluno> feminino = new ArrayList<>();
+        List<Aluno> masculino = new ArrayList<>();
+        Scanner scan = new Scanner(System.in);
+
+        for (int i = 0; i < 4; i++) {
+            String nomeDigitado;
+            String sexoDigitado;
+
+//            coleta dados do usuário
+            System.out.print("Digite o nome: ");
+            nomeDigitado = scan.next();
+            System.out.print("Digite o sexo: ");
+            sexoDigitado = scan.next();
+            Aluno aluno = new Aluno(nomeDigitado, sexoDigitado);
+
+//            valida sexo
+            if (aluno.getSexo().equals("m")) {
+                masculino.add(aluno);
+            } else if (aluno.getSexo().equals("f")) {
+                feminino.add(aluno);
+            } else {
+                System.out.println("Sexo digitado é inválido!");
+            }
+        }
+        System.out.println("Feminino:  " + feminino);
+        System.out.println("Masculino: " + masculino);
+    }
+}
---
+++ Engenheiro Back-end Java/11 - Coleções - Parte 1/ExercicioMod11/src/br/com/escuderodev/models/Aluno.java
@@ -0,0 +1,42 @@
+package br.com.escuderodev.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Aluno implements Comparable<Aluno> {
+    private String nome;
+    private String sexo;
+
+    public Aluno (String nome, String sexo) {
+        this.nome = nome;
+        this.sexo = sexo;
+    }
+
+//    getters and setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    @Override
+    public int compareTo(Aluno aluno) {
+        return this.sexo.compareTo(aluno.getSexo());
+    }
+
+    @Override
+    public String toString() {
+        return this.nome + " " + this.sexo;
+    }
+
+}
