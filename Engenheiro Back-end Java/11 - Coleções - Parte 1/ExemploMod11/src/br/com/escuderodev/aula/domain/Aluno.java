87f6c4ab6167c484ed06560da942d42ff7f7bfd1
Eduardo Escudero <escudero.ti@gmail.com>
2023-01-07 14:02:00 -0300

    att

--- Engenheiro Back-end Java/11 - Coleções - Parte 1/ExemploMod11/src/br/com/escuderodev/aula/domain/Aluno.java
+++ Engenheiro Back-end Java/11 - Coleções - Parte 1/ExemploMod11/src/br/com/escuderodev/aula/domain/Aluno.java
@@ -1,5 +1,7 @@
 package br.com.escuderodev.aula.domain;
 
+import java.util.Objects;
+
 public class Aluno implements Comparable<Aluno> {
     private String nome;
     private String curso;
@@ -71,7 +73,20 @@ public class Aluno implements Comparable<Aluno> {
     //    método para comparar dois objeto e verificar igualdade
     @Override
     public int compareTo(Aluno aluno) {
+
         return this.nome.compareTo(aluno.getNome());
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Aluno aluno = (Aluno) o;
+        return Double.compare(aluno.nota, nota) == 0 && Objects.equals(nome, aluno.nome) && Objects.equals(curso, aluno.curso) && Objects.equals(sala, aluno.sala);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, curso, nota, sala);
+    }
 }
