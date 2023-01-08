e302a109ad363f3292fe06fab2a6c9c06d1e5bf8
EDUARDO ESCUDERO <88359144+escuderodev@users.noreply.github.com>
2023-01-08 15:29:25 -0300

    att

---
+++ Engenheiro Back-end Java/12 - Arrays/ExemploMod12/src/br/com/escuderodev/aula/array/TestaCriacaoArray.java
@@ -0,0 +1,23 @@
+package br.com.escuderodev.aula.array;
+
+import java.util.Scanner;
+
+public class TestaCriacaoArray {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int[] numeros = new int[10];
+
+//        inserindo elementos no array via teclado
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Digite um número: ");
+            numeros[i] = scan.nextInt();
+        }
+
+//        percorrendo o array
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print(numeros[i] + " ");
+        }
+    }
+
+}
