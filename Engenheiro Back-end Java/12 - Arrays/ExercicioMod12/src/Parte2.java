9033414ab7094167a5468ee1545339c3150ee7d6
Eduardo Escudero <escudero.ti@gmail.com>
2023-01-21 13:28:25 -0300

    concluindo exercicio mod 12

---
+++ Engenheiro Back-end Java/12 - Arrays/ExercicioMod12/src/Parte2.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Parte2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String nomesDigitados;
+        String[] nomes = new String[4];
+        String[] masculino = new String[4];
+        String[] feminino = new String[4];
+
+        System.out.print("Digite o nome e o gênero de 4 pessoas separados por - e , : ");
+        nomesDigitados = scan.next();
+        nomes = nomesDigitados.split(",");
+
+        for (int i = 0; i < nomes.length; i++) {
+            if (nomes[i].contains("-m")){
+                masculino[i] = nomes[i];
+            } else {
+                feminino[i] = nomes[i];
+            }
+        }
+
+        System.out.println("Imprimindo pessoas do sexo masculino:");
+        for (int i = 0; i < masculino.length; i++){
+            if (masculino[i] != null)
+            System.out.println(masculino[i]);
+        }
+
+        System.out.println("Imprimindo pessoas do sexo feminino:");
+        for (int i = 0; i < masculino.length; i++){
+            if (feminino[i] != null) {
+                System.out.println(feminino[i]);
+            }
+        }
+    }
+}
