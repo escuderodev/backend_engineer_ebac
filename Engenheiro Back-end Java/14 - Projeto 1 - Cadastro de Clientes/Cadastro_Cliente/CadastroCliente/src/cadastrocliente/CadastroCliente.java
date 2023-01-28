1360ae945d0a6836f05971c04871149a7573e968
EDUARDO ESCUDERO <88359144+escuderodev@users.noreply.github.com>
2023-01-28 18:45:36 -0300

    paramos na aula 14.4

---
+++ Engenheiro Back-end Java/14 - Projeto 1 - Cadastro de Clientes/Cadastro_Cliente/CadastroCliente/src/cadastrocliente/CadastroCliente.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cadastrocliente;
+
+/**
+ *
+ * @author escud
+ */
+public class CadastroCliente {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//        instanciando a tela principal
+        TelaPrincipal telaPrincipal = new TelaPrincipal();
+        telaPrincipal.setVisible(true);
+
+    }
+    
+}
---
+++ Engenheiro Back-end Java/14 - Projeto 1 - Cadastro de Clientes/Cadastro_Cliente/CadastroCliente/src/dao/IClienteDAO.java
@@ -0,0 +1,14 @@
+package br.com.escuderodev.dao;
+
+import br.com.escuderodev.domain.Cliente;
+
+import java.util.Collection;
+
+public interface IClienteDAO {
+
+    public Boolean cadastrar(Cliente cliente);
+    public void excluir(Long cpf);
+    public void alterar(Cliente cliente);
+    public Cliente consultar(Long cpf);
+    public Collection<Cliente> buscarTodos();
+}
