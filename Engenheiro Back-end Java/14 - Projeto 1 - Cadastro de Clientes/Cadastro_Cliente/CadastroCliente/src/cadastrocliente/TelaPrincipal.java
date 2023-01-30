6bcf2c130c0e5bbc7b6b1034703d8e2fff4e501d
EDUARDO ESCUDERO <88359144+escuderodev@users.noreply.github.com>
2023-01-30 19:38:40 -0300

    concluímos o projeto mod14

--- Engenheiro Back-end Java/14 - Projeto 1 - Cadastro de Clientes/Cadastro_Cliente/CadastroCliente/src/cadastrocliente/TelaPrincipal.java
+++ Engenheiro Back-end Java/14 - Projeto 1 - Cadastro de Clientes/Cadastro_Cliente/CadastroCliente/src/cadastrocliente/TelaPrincipal.java
@@ -54,6 +54,7 @@ public class TelaPrincipal extends javax.swing.JFrame {
         btnSalvar = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         tbClientes = new javax.swing.JTable();
+        btnExcluir = new javax.swing.JButton();
         jMenuBar1 = new javax.swing.JMenuBar();
         menuOpcoes = new javax.swing.JMenu();
         opcaoSair = new javax.swing.JMenuItem();
@@ -94,7 +95,19 @@ public class TelaPrincipal extends javax.swing.JFrame {
                 "Title 1", "Title 2", "Title 3", "Title 4"
             }
         ));
+        tbClientes.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                tbClientesMouseClicked(evt);
+            }
+        });
         jScrollPane1.setViewportView(tbClientes);
+
+        btnExcluir.setText("EXCLUIR");
+        btnExcluir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnExcluirActionPerformed(evt);
+            }
+        });
 
         menuOpcoes.setText("Opções");
 
@@ -149,8 +162,10 @@ public class TelaPrincipal extends javax.swing.JFrame {
                                             .addComponent(txtNumero, javax.swing.GroupLayout.PREFERRED_SIZE, 106, javax.swing.GroupLayout.PREFERRED_SIZE)
                                             .addGroup(layout.createSequentialGroup()
                                                 .addComponent(txtEstado, javax.swing.GroupLayout.PREFERRED_SIZE, 494, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                                .addGap(130, 130, 130)
-                                                .addComponent(btnSalvar, javax.swing.GroupLayout.PREFERRED_SIZE, 93, javax.swing.GroupLayout.PREFERRED_SIZE)))))))))
+                                                .addGap(18, 18, 18)
+                                                .addComponent(btnSalvar, javax.swing.GroupLayout.PREFERRED_SIZE, 93, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                .addGap(18, 18, 18)
+                                                .addComponent(btnExcluir, javax.swing.GroupLayout.PREFERRED_SIZE, 93, javax.swing.GroupLayout.PREFERRED_SIZE)))))))))
                 .addContainerGap(43, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -188,7 +203,8 @@ public class TelaPrincipal extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(txtEstado, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(lbNome5, javax.swing.GroupLayout.DEFAULT_SIZE, 36, Short.MAX_VALUE)
-                    .addComponent(btnSalvar, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(btnSalvar, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnExcluir, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(27, 27, 27)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 487, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(44, 44, 44))
@@ -232,6 +248,40 @@ public class TelaPrincipal extends javax.swing.JFrame {
                
     }//GEN-LAST:event_btnSalvarActionPerformed
 
+    private void tbClientesMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tbClientesMouseClicked
+       int linhaSelecionada = tbClientes.getSelectedRow();
+       Long cpf = (Long) tbClientes.getValueAt(linhaSelecionada, 1);
+       
+       Cliente cliente = this.clientDAO.consultar(cpf);
+       
+       txtNome.setText(cliente.getNome());
+       txtCPF.setText(cliente.getCpf().toString());
+       txtCidade.setText(cliente.getCidade());
+       txtTelefone.setText(cliente.getTel().toString());
+       txtEstado.setText(cliente.getEstado());
+       txtEndereco.setText(cliente.getEndereco());
+       txtNumero.setText(cliente.getNumero().toString());       
+    }//GEN-LAST:event_tbClientesMouseClicked
+
+    private void btnExcluirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnExcluirActionPerformed
+        int linhaSelecionada = tbClientes.getSelectedRow();
+        
+        if (linhaSelecionada >= 0) {
+            int result = JOptionPane.showConfirmDialog(this, "Deseja realmente excluir este cliente?", "Cuidado", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+            
+            if (result == JOptionPane.YES_OPTION) {
+                Long cpf = (Long) tbClientes.getValueAt(linhaSelecionada, 1);
+                this.clientDAO.excluir(cpf);
+                modelo.removeRow(linhaSelecionada);
+                
+                JOptionPane.showMessageDialog(null, "Cliente excluído com sucesso!", "Sucesso", JOptionPane.INFORMATION_MESSAGE);
+                limparCampos();
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "Nenhum cliente selecionado.", "ERRO", JOptionPane.INFORMATION_MESSAGE);
+        }
+    }//GEN-LAST:event_btnExcluirActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -268,6 +318,7 @@ public class TelaPrincipal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnExcluir;
     private javax.swing.JButton btnSalvar;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JMenuBar jMenuBar1;
