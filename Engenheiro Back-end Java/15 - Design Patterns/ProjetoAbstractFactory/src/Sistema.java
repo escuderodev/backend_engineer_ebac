0696b758bf73d9197039b50b3125581f0d4d1c56
escuderodev <escudero.ti@gmail.com>
2023-02-10 19:38:25 -0300

    concluímos o projeto do mod 15

---
+++ Engenheiro Back-end Java/15 - Design Patterns/ProjetoAbstractFactory/src/Sistema.java
@@ -0,0 +1,18 @@
+import br.com.escuderodev.domain.*;
+
+public class Sistema {
+    public static void main(String[] args) {
+        Cliente cliente = new Cliente("A", true);
+        LocadoraDeCarro locadoraDeCarro = getTipoCarro(cliente);
+        Carro carro = locadoraDeCarro.criaCarro(cliente.getOpcaoCarro());
+        carro.partiuViagem();
+    }
+
+    public static LocadoraDeCarro getTipoCarro(Cliente cliente) {
+        if (cliente.ehCarroExecutivo()) {
+            return new AlugaCarroExecutivo();
+        } else {
+            return new AlugaCarroPopular();
+        }
+    }
+}
