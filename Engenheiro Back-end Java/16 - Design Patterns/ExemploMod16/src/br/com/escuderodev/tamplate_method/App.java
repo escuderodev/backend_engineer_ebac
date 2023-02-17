7977d71a40562ede7c140ffe6308058b7f58aeff
escuderodev <escudero.ti@gmail.com>
2023-02-17 19:46:16 -0300

    att docs

---
+++ Engenheiro Back-end Java/16 - Design Patterns/ExemploMod16/src/br/com/escuderodev/tamplate_method/App.java
@@ -0,0 +1,10 @@
+package br.com.escuderodev.tamplate_method;
+
+public class App {
+    public static void main(String[] args) {
+        Trabalhador trabalhador = new Advogado();
+
+        trabalhador.executarRotina();
+
+    }
+}
